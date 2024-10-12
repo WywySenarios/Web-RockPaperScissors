96b61025197bd184578b2f18c6bca2120c5e5426
WywySenarios <wywyericzhu@gmail.com>
2024-10-12 11:39:56 -0400

    basic functionality

--- run.js
+++ run.js
@@ -3,26 +3,70 @@
  */
 
 function defaultTestCases() {
-    return [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3]];
+	return [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
+}
+
+function testCases() {
+	return defaultTestCases();
 }
 
 function run() {
-    let runtime = 0;
-    for (let i = 0; i < 100; i++) {
-        runtime += RPSstandard(defaultTestCases());
-    }
-    console.log(`${runtime}ms`);
-    // setTimeout(() => {RPSstandard(defaultTestCases())}, 1000);
+	let results = Array(0);
+	let runtime = 0;
+	let games = testCases();
+	let start;
+	let end;
 
-    // μ
+	// standard
+	start = performance.now();
+	results.push(RPSstandard(games));
+	end = performance.now();
+	runtime = end - start;
+	console.log(`${runtime}ms`);
+	console.log(results[0]);
+	sleep(1000)
+
+	// optimized
+	start = performance.now();
+	results.push(RPSoptimized(games));
+	end = performance.now();
+	runtime = end - start;
+	console.log(`${runtime}ms`);
+	console.log(results[0]);
+	sleep(1000);
+
+
+	// map
+	start = performance.now();
+	results.push(RPSmap(games));
+	end = performance.now();
+	runtime = end - start;
+	console.log(`${runtime}ms`);
+	console.log(results[0]);
+	sleep(1000);
+
+	// noComparisons AKA Polynomial
+	start = performance.now();
+	results.push(RPSnoComparisons(games));
+	end = performance.now();
+	runtime = end - start;
+	console.log(`${runtime}ms`);
+	console.log(results[0]);
+	sleep(1000);
+	// μ
 }
 
-// Returns runtime in ms
+function sleep(ms) {
+	let a = new Promise(resolve => setTimeout(resolve, ms));
+	a.then(() => {return;});
+}
+
+// Returns an array with the execution results
 function RPSstandard(games) {
-    let results = Array(0);
-    const start = performance.now();
-    for (let i = 0; i < games.length; i++) {
-        if (games[i][0] == games[i][1]) { // tie
+	let results = Array(0);
+
+	for (let i = 0; i < games.length; i++) {
+		if (games[i][0] == games[i][1]) { // tie
 			results.push(0);
 		} else if (games[i][0] == 1) { // p1 rock
 			if (games[i][1] == 2) { // p2 papers
@@ -43,17 +87,16 @@ function RPSstandard(games) {
 				results.push(2);
 			}
 		}
-    }
+	}
 
-    const end = performance.now();
-    return end - start;
+	return results;
 }
 
-// Returns runtime in ms
+// Returns an array with the execution results
 function RPSoptimized(games) {
-    let results = Array(0);
-    const start = performance.now();
-    for (let i = 0; i < games.length; i++) {
+	let results = Array(0);
+
+	for (let i = 0; i < games.length; i++) {
 		if (games[i][0] == games[i][1]) { // tie
 			results.push(0);
 		} else if (games[i][0] == (-3 * games[i][1] * games[i][1] + 11 * games[i][1] - 4) / 2) { // P1 wins
@@ -61,19 +104,37 @@ function RPSoptimized(games) {
 			// this means that the quadratic equation has points (1,3), (2,1), and (3,2)
 			results.push(1);
 		} else {
-            results.push(2);
+			results.push(2);
 		}
-    }
-    
-    const end = performance.now();
-    return end - start;
+	}
+
+	return results;
 }
 
-// Returns runtime in ms
+// Returns an array with the execution results
+function RPSmap(games) {
+	let results = Array(0);
+
+	for (let i = 0; i < games.length; i++) {
+		let game = games[i][0] * 10 + games[i][1]; // [p1][p2]
+
+		if ((game - 11) * (game - 22) * (game - 33) == 0) { // tie
+			results.push(0);
+		} else if ((game - 13) * (game - 21) * (game - 32) == 0) { // P1 wins
+			results.push(1);
+		} else {
+			results.push(2);
+		}
+	}
+
+	return results;
+}
+
+// Returns an array with the execution results
 function RPSnoComparisons(games) {
-    let results = Array(0);
-    const start = performance.now();
-    for (let i = 0; i < games.length; i++) {
+	let results = Array(0);
+
+	for (let i = 0; i < games.length; i++) {
 		/*
 		 * Polynomial equation that satisfies the following points:
 		 * (1,1,0) (2,2,0) (3,3,0)
@@ -82,10 +143,9 @@ function RPSnoComparisons(games) {
 		 */
 		let x = games[i][0];
 		let z = games[i][1];
-		
-		results.push(Math.round((x - z) *(x + -2.66069 * z) * (x + -0.753293 * z) * (x + -2.96691 * z) * (x + -0.751818 * z) * (x + -1.48948 * z) * (x + -0.33313 * z) * (x + -0.751626 * z) * (x + -1.05309 * z)));
-    }
-    
-    const end = performance.now();
-    return end - start;
+
+		results.push(Math.round((x - z) * (x + -2.66069 * z) * (x + -0.753293 * z) * (x + -2.96691 * z) * (x + -0.751818 * z) * (x + -1.48948 * z) * (x + -0.33313 * z) * (x + -0.751626 * z) * (x + -1.05309 * z)));
+	}
+
+	return results;
 }
