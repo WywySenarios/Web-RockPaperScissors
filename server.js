4194eb769ccfd8bc8dce5b61fd1c8be44237b525
WywySenarios <wywyericzhu@gmail.com>
2024-10-14 23:15:12 -0400

    randomization attempt
    TRIED to add C++ (failed & aborted)
    
    TRIED to randomize (failed to randomize server-side, very sad)
    
    TRIED to GET (failed because there was no success server-side on generating output)
    
    Added a "public" folder so that the client can't access server files

--- server.js
+++ server.js
@@ -12,24 +12,36 @@ function bootQuestion() {
   });
 }
 
+function randomizeTestCases(numGames) {
+  let output = "";
+
+  for (let i = 0; i < numGames; i++) {
+    output += `${1 + Math.round(Math.random() * 2)}${Math.round(1 + Math.random() * 2)}`;
+  }
+
+  return output;
+}
+
+function shuffleTestCases() {}
+
 function boot() {
+  // imports
   const express = require('express');
   const cors = require('cors');
   const multer = require('multer');
   const bodyParser = require('body-parser');
+  //const cppAddon = require('.build/Release/randomizeTestCases');
+
+  //console.log(cppAddon.hello());
+
+  // important constants
   const port = 5322;
   const app = express();
 
   app.use(cors()); // allow input from ANY ip address
   app.use(bodyParser.json()); // allow JSON input (related to the POST function)
 
-  // const urlEncodedParams = express.urlencoded({
-  //   extended : false, // use complex algorithm for large amounts of nested data?
-  //   limit: 10000, // limit in bit thingies (a 0 or 1)
-  //   parameterLimit: 5 // max # items
-  // });
-
-  app.use(express.static(__dirname));
+  app.use(express.static(__dirname + '/public')); // allow file access in root directory
 
   app.use(express.urlencoded({
     extended: false, // use complex algorithm for large amounts of nested data?
@@ -39,27 +51,22 @@ function boot() {
 
   const upload = multer({ dest: 'files/' });
 
-  // get commands
-  // app.get("/", (req, res) => {
-  //   console.log("GET command received.");
-  //   res.sendFile("index.html", {root: __dirname});
-  // });
-
-  app.post('/', (req, res) => {
-    console.log("Empty Post");
+  app.get('/testCases', (req, res) => {
+    let testCases = randomizeTestCases();
+    console.log(`Sending test cases: ${testCases}`);
+    res.send(testCases.toString("utf-8"));
+    res.status(200);
   });
 
   app.post('/upload', (req, res) => {
     // handle upload
-    console.log("Receiving Data:");
-    // console.log(req);
-    console.log(req.body);
-    // console.log(req.files);
-    console.log("END: Receiving Data");
+    console.log(`Receiving Data:${req.body}:END-Receiving Data`); // note what input has been given in logs
+
+    res.status(204);
   });
 
   app.listen(port, () => {
-    console.log(`Server online at port ${port}.`)
+    console.log(`Server online at port ${port}.`);
   });
 }
 
